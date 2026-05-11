ce59790ed9de6a01510f62309a556c997623bbae
Sobhan Maity <85999157+iamSobhan@users.noreply.github.com>
2026-05-11 17:14:21 +0530

    check odd or even

---
+++ Object-Oriented Programming/even_odd.cpp
@@ -0,0 +1,19 @@
+// Check Even or Odd
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num = 8;
+
+    if(num % 2 == 0)
+        cout << "Even";
+    else
+        cout << "Odd";
+
+    return 0;
+}
+
+
+// % gives a remainder, if remainder is 0 - even.
