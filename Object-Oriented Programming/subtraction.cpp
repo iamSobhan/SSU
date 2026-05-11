93adb701a9329860b5452a6904c7c59256914fb9
Sobhan Maity <85999157+iamSobhan@users.noreply.github.com>
2026-05-11 17:03:34 +0530

    subtraction

---
+++ Object-Oriented Programming/subtraction.cpp
@@ -0,0 +1,15 @@
+// Subtraction 
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 30, b = 10;
+    int sub = a - b;
+
+    cout << "Sub = " << sub;
+    return 0;
+}
+
+
+// We subtracts two integers using -.
