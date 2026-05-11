3190b9c409eb7e78740124ac5e73509c68f033ac
Sobhan Maity <85999157+iamSobhan@users.noreply.github.com>
2026-05-11 17:09:16 +0530

    swapping

---
+++ Object-Oriented Programming/swap_two_numbers.cpp
@@ -0,0 +1,22 @@
+// Swap Two Numbers
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5, b = 10, temp;
+
+    temp = a;
+    a = b;
+    b = temp;
+
+    cout << "a = " << a << endl;
+    cout << "b = " << b;
+
+    return 0;
+}
+
+
+
+// Here a temporary variable is used for swapping.
