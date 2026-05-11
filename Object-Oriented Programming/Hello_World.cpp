e2b71522dbf98b8475d141db4ae97fd2879d8fc9
Sobhan Maity <85999157+iamSobhan@users.noreply.github.com>
2026-05-11 16:24:45 +0530

    hello_world

---
+++ Object-Oriented Programming/Hello_World.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Computer Applications";
+    return 0;
+}
+
+
+// Here cout is used to print output.
+// return 0 means program executed successfully.
+
