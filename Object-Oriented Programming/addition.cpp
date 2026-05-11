00596f255348ad2150b2f1a67d4c57d3113a3a44
Sobhan Maity <85999157+iamSobhan@users.noreply.github.com>
2026-05-11 16:59:59 +0530

    addition

---
+++ Object-Oriented Programming/addition.cpp
@@ -0,0 +1,16 @@
+// Addition of Two Numbers
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 10, b = 20;
+    int sum = a + b;
+
+    cout << "Sum = " << sum;
+    return 0;
+}
+
+
+
+// We added two integers using +.
