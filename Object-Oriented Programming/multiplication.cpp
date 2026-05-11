2db337996c092d1d6e84d79ba8f475b543da1028
Sobhan Maity <85999157+iamSobhan@users.noreply.github.com>
2026-05-11 16:59:15 +0530

    multiplication

---
+++ Object-Oriented Programming/multiplication.cpp
@@ -0,0 +1,16 @@
+// 	Multiplication
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 30, b = 50;
+    int mul = a * b;
+
+    cout << "Mul = " << mul;
+    return 0;
+}
+
+
+// We multiplies two integers using *.
