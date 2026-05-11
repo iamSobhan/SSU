3a431b9c3fac520c6ed0c73ad983761a7abedfcb
Sobhan Maity <85999157+iamSobhan@users.noreply.github.com>
2026-05-11 17:05:39 +0530

    division

---
+++ Object-Oriented Programming/division.cpp
@@ -0,0 +1,16 @@
+// 	Division
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 30, b = 5;
+    int div = a / b;
+
+    cout << "Div = " << div;
+    return 0;
+}
+
+
+// We divides two integers using /.
