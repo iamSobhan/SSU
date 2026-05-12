845995106a5b452ac0ba9f1c6614749f00f69f9d
Sobhan Maity <85999157+iamSobhan@users.noreply.github.com>
2026-05-12 09:57:33 +0530

    multiplication_table

---
+++ Object-Oriented Programming/multiplication_table.cpp
@@ -0,0 +1,15 @@
+// Multiplication Table
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 5;
+
+    for(int i = 1; i <= 10; i++) {
+        cout << n << " x " << i << " = " << n*i << endl;
+    }
+
+    return 0;
+}
