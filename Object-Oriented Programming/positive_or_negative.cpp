51f79e816e255f46aad0d7dc2bb3a31443c91ea2
Sobhan Maity <85999157+iamSobhan@users.noreply.github.com>
2026-05-12 10:31:57 +0530

    positive_or_negative

---
+++ Object-Oriented Programming/positive_or_negative.cpp
@@ -0,0 +1,16 @@
+// Check Positive or Negative
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num = -5;
+
+    if(num >= 0)
+        cout << "This number is Positive";
+    else
+        cout << "This number is Negative";
+
+    return 0;
+}
