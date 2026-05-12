65eab4e68d1b3ac47fd269905d3109bacfb97237
Sobhan Maity <85999157+iamSobhan@users.noreply.github.com>
2026-05-12 09:52:20 +0530

    simple_calculator

---
+++ Object-Oriented Programming/calculator.cpp
@@ -0,0 +1,16 @@
+// Simple Calculator
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 10, b = 5;
+
+    cout << "Addition = " << a + b << endl;
+    cout << "Subtraction = " << a - b << endl;
+    cout << "Multiplication = " << a * b << endl;
+    cout << "Division = " << a / b;
+
+    return 0;
+}
