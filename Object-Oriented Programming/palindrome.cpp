2e23d74b3c9d3a0e7d822317a6cc0bc3c28b0ca7
Sobhan Maity <85999157+iamSobhan@users.noreply.github.com>
2026-05-12 10:14:36 +0530

    checking_palindrome

---
+++ Object-Oriented Programming/palindrome.cpp
@@ -0,0 +1,23 @@
+// Checking Palindrome Number
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num = 121, temp, reverse = 0;
+
+    temp = num;
+
+    while(num != 0) {
+        reverse = reverse * 10 + num % 10;
+        num /= 10;
+    }
+
+    if(temp == reverse)
+        cout << "Palindrome";
+    else
+        cout << "Not Palindrome";
+
+    return 0;
+}
