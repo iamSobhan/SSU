9664c6b24c134f3d94c86469fe3ab5749f0874d3
Sobhan Maity <85999157+iamSobhan@users.noreply.github.com>
2026-05-12 10:04:40 +0530

    factorial_of_a_no

---
+++ Object-Oriented Programming/factorial.cpp
@@ -0,0 +1,17 @@
+// Factorial of a Number
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 5, fact = 1;
+
+    for(int i = 1; i <= n; i++) {
+        fact *= i;
+    }
+
+    cout << "Factorial = " << fact;
+
+    return 0;
+}
