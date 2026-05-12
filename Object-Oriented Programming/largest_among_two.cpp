dfa3f17d45191cb683f40a5f90328a538a5370ee
Sobhan Maity <85999157+iamSobhan@users.noreply.github.com>
2026-05-12 09:50:56 +0530

    largest_no

---
+++ Object-Oriented Programming/largest_among_two.cpp
@@ -0,0 +1,19 @@
+// Find Largest Number
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 15, b = 25;
+
+    if(a > b)
+        cout << a << " is largest";
+    else
+        cout << b << " is largest";
+
+    return 0;
+}
+
+
+// if-else compares two numbers.
