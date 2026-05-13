6466a39e0f75ff1f8d695990efc4815dc741f371
Sobhan Maity <85999157+iamSobhan@users.noreply.github.com>
2026-05-13 09:52:36 +0530

    checking_length_of_a_string

---
+++ Object-Oriented Programming/string_length.cpp
@@ -0,0 +1,14 @@
+// Write a C++ program to find the length of a string.
+
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main() {
+    char str[] = "Bachelor of Computer Applications";
+
+    cout << "Length = " << strlen(str);
+
+    return 0;
+}
