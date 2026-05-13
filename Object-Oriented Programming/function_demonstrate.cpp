acede5b1f6a189af2adc219acf2983d905dbea30
Sobhan Maity <85999157+iamSobhan@users.noreply.github.com>
2026-05-13 10:33:00 +0530

    function_demonstrate

---
+++ Object-Oriented Programming/function_demonstrate.cpp
@@ -0,0 +1,15 @@
+// Write a C++ program to demonstrate the use of functions.
+
+
+#include <iostream>
+using namespace std;
+
+int add(int a, int b) {
+    return a + b;
+}
+
+int main() {
+    cout << "Sum = " << add(5, 3);
+
+    return 0;
+}
