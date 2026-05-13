b48f5d9386fc2ad88da7837f1dc42c75f80529f2
Sobhan Maity <85999157+iamSobhan@users.noreply.github.com>
2026-05-13 09:30:00 +0530

    calculate_area_of_a_circle

---
+++ Object-Oriented Programming/circle_area.cpp
@@ -0,0 +1,15 @@
+// Find area of a Circle
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    float r = 5, area;
+
+    area = 3.14 * r * r;
+
+    cout << "Area = " << area;
+
+    return 0;
+}
