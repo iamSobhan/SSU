1b4aa4b221f04f46a2af0836d568d8a2f71cc821
Sobhan Maity <85999157+iamSobhan@users.noreply.github.com>
2026-05-27 20:36:47 +0530

    number_square

---
+++ Object-Oriented Programming/square_of_a_number.cpp
@@ -0,0 +1,13 @@
+// Write a C++ program to find the square of a number.
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 5;
+
+    cout << "Square = " << n * n;
+
+    return 0;
+}
