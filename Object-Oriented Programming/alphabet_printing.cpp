22c053cd96a378c3af3931a7a18ea2310a3731ba
Sobhan Maity <85999157+iamSobhan@users.noreply.github.com>
2026-05-27 21:30:48 +0530

    printing_alphabets

---
+++ Object-Oriented Programming/alphabet_printing.cpp
@@ -0,0 +1,14 @@
+// Write a C++ program to print the alphabet from A to Z.
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    for(char ch = 'A'; ch <= 'Z'; ch++) {
+        cout << ch << " ";
+    }
+
+    return 0;
+}
