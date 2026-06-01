e644c49f0fb53ac98ea38fb657ef75f96dcc991c
Sobhan Maity <85999157+iamSobhan@users.noreply.github.com>
2026-06-01 17:08:14 +0530

    stack_implementation

---
+++ Object-Oriented Programming/stack.cpp
@@ -0,0 +1,18 @@
+// Write a C++ program to demonstrate the use of stack.
+
+
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main() {
+    stack<int> s;
+
+    s.push(10);
+    s.push(20);
+    s.push(30);
+
+    cout << s.top();
+
+    return 0;
+}
