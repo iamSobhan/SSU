e9b2cd8c081be08653942a529e6cf9047d8ae025
Sobhan Maity <85999157+iamSobhan@users.noreply.github.com>
2026-06-02 12:25:27 +0530

    checking equal strings

---
+++ Object-Oriented Programming/equal_strings.cpp
@@ -0,0 +1,17 @@
+// Write a C++ program to check whether two strings are equal.
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    string s1 = "Computer";
+    string s2 = "Computer";
+
+    if(s1 == s2)
+        cout << "Strings are Equal";
+    else
+        cout << "Strings are Not Equal";
+
+    return 0;
+}
