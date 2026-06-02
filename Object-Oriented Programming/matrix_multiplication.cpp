77e7d4f9063fe0a4989d4a73e76da533097662f8
Sobhan Maity <85999157+iamSobhan@users.noreply.github.com>
2026-06-02 12:27:55 +0530

    matrix multiplication implementation

---
+++ Object-Oriented Programming/matrix_multiplication.cpp
@@ -0,0 +1,32 @@
+// Write a C++ program to perform matrix multiplication.
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a[2][2] = {{1,2},{3,4}};
+    int b[2][2] = {{5,6},{7,8}};
+    int result[2][2];
+
+    for(int i=0; i<2; i++) {
+        for(int j=0; j<2; j++) {
+            result[i][j] = 0;
+
+            for(int k=0; k<2; k++) {
+                result[i][j] += a[i][k] * b[k][j];
+            }
+        }
+    }
+
+    cout << "Matrix Multiplication:" << endl;
+
+    for(int i=0; i<2; i++) {
+        for(int j=0; j<2; j++) {
+            cout << result[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
