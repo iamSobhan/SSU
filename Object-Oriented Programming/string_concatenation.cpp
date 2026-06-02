5492acfb5169283e94af96750f713eb3ad963242
Sobhan Maity <85999157+iamSobhan@users.noreply.github.com>
2026-06-02 12:32:45 +0530

    string concatenation implementation

---
+++ Object-Oriented Programming/string_concatenation.cpp
@@ -0,0 +1,15 @@
+// Write a C++ program to concatenate strings.
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    string s1 = "Seacom ";
+    string s2 = "Skills ";
+    string s3 = "University";
+
+    cout << s1 + s2 + s3;
+
+    return 0;
+}
