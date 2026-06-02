aa03b4abdf8856bbfd1669105408b672d81ef20e
Sobhan Maity <85999157+iamSobhan@users.noreply.github.com>
2026-06-02 12:20:25 +0530

    characters lowercase to uppercase implementation

---
+++ Object-Oriented Programming/characters_lowercase_to_uppercase.cpp
@@ -0,0 +1,15 @@
+// Write a C++ program to convert lowercase characters to uppercase.
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    char ch = 'a';
+
+    ch = ch - 32;
+
+    cout << "Uppercase = " << ch;
+
+    return 0;
+}
