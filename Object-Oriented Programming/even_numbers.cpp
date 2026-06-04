717ff563af3fc828cb6bc581d2f93bb8b9c67ec5
Sobhan Maity <85999157+iamSobhan@users.noreply.github.com>
2026-06-04 21:29:09 +0530

    printing even numbers

---
+++ Object-Oriented Programming/even_numbers.cpp
@@ -0,0 +1,15 @@
+// Write a C++ program to print even numbers from 1 to 20.
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    for(int i = 1; i <= 20; i++) {
+        if(i % 2 == 0)
+            cout << i << " ";
+    }
+
+    return 0;
+}
