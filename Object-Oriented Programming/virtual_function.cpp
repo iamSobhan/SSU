f4c0b140f71854470b0efcaf1408c59094fcf7cd
Sobhan Maity <85999157+iamSobhan@users.noreply.github.com>
2026-06-04 12:49:45 +0530

    virtual function implementation

---
+++ Object-Oriented Programming/virtual_function.cpp
@@ -0,0 +1,29 @@
+// Write a C++ program to demonstrate virtual function.
+
+
+#include <iostream>
+using namespace std;
+
+class Base {
+public:
+    virtual void show() {
+        cout << "Base Class";
+    }
+};
+
+class Derived : public Base {
+public:
+    void show() {
+        cout << "Derived Class";
+    }
+};
+
+int main() {
+    Base *b;
+    Derived d;
+
+    b = &d;
+    b->show();
+
+    return 0;
+}
